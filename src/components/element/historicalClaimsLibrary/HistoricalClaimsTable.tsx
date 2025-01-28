b9f5dc9528124a74fd45b09340a7d9a9f725abae
Viande <viande@double.cat>
2025-01-28 13:00:10 +0100

    make the table responsive

--- src/components/element/historicalClaimsLibrary/HistoricalClaimsTable.tsx
+++ src/components/element/historicalClaimsLibrary/HistoricalClaimsTable.tsx
@@ -3,28 +3,22 @@ import { createTable } from "dappkit";
 export const [HistoricalClaimsTable, HistoricalClaimsRow, HistoricalClaimsColumns] = createTable({
   chain: {
     name: "Chain",
-    size: "minmax(120px,150px)",
-    compact: "1fr",
+    size: "minmax(150px, 1fr)",
     className: "justify-start",
-    main: true,
   },
   token: {
     name: "Token",
-    size: "minmax(120px,150px)",
-    compact: "1fr",
+    size: "minmax(150px, 1fr)",
     className: "justify-start",
-    main: true,
   },
   date: {
     name: "Date",
-    size: "minmax(30px,1fr)",
-    compactSize: "minmax(20px,1fr)",
+    size: "minmax(120px, 1fr)",
     className: "justify-end",
   },
   transaction: {
     name: "Transaction",
-    size: "minmax(30px,1fr)",
-    compactSize: "minmax(20px,0.5fr)",
+    size: "minmax(100px, 1fr)",
     className: "justify-end",
   },
 });
