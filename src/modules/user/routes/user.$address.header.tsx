21c4941f39f2ac7ef4b85ba3a75444e4ae3823b6
sheykei <clement4saunier@gmail.com>
2025-02-11 18:46:36 +0100

    add

--- src/modules/user/routes/user.$address.header.tsx
+++ src/modules/user/routes/user.$address.header.tsx
@@ -85,7 +85,7 @@ export default function Index() {
           </>
         ),
         link: `/users/${address}`,
-        key: crypto.randomUUID(),
+        key: "user-link",
       },
       {
         label: (
@@ -105,7 +105,7 @@ export default function Index() {
           </>
         ),
         link: `/users/${address}/claims`,
-        key: crypto.randomUUID(),
+        key: "user-claims",
       },
     ];
 
