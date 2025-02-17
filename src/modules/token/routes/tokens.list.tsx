fb46378f808193eafd3daabd65265f59da2dc99b
sheykei <clement4saunier@gmail.com>
2025-02-17 10:46:39 +0100

    fix: lint

--- src/modules/token/routes/tokens.list.tsx
+++ src/modules/token/routes/tokens.list.tsx
@@ -1,9 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { useNavigate } from "@remix-run/react";
 import { Container, Space } from "dappkit";
-import { useEffect } from "react";
-import merklConfig from "../../../config";
 import { Cache } from "../../../modules/cache/cache.service";
 import { TokenService } from "../../../modules/token/token.service";
 import TokenLibrary from "../components/library/TokenLibrary";
