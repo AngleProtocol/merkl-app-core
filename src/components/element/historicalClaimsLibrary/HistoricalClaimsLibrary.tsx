89849d96645f92f87356fc058c15e3e9380af713
sheykei <clement4saunier@gmail.com>
2025-03-03 11:52:30 +0100

    update: historyu

--- src/components/element/historicalClaimsLibrary/HistoricalClaimsLibrary.tsx
+++ src/components/element/historicalClaimsLibrary/HistoricalClaimsLibrary.tsx
@@ -1,9 +1,12 @@
-import { Group, Text, Title } from "dappkit";
-import { useMemo } from "react";
+import { Box, Group, Icon, Select, Space, Text, Title, useWalletContext } from "dappkit";
+import { useCallback, useMemo, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import type { ClaimsService } from "../../../modules/claim/claim.service";
 import HistoricalClaimsTableRow from "./HistoricalClaimsRow";
 import { HistoricalClaimsTable } from "./HistoricalClaimsTable";
+import useChains from "@core/modules/chain/hooks/useChains";
+import useSearchParamState from "@core/hooks/filtering/useSearchParamState";
+import { useSearchParams } from "@remix-run/react";
 
 export type HistoricalClaimsLibraryProps = {
   claims: Awaited<ReturnType<typeof ClaimsService.getForUser>>;
@@ -11,26 +14,45 @@ export type HistoricalClaimsLibraryProps = {
 
 export default function HistoricalClaimsLibrary(props: HistoricalClaimsLibraryProps) {
   const { claims } = props;
-  // const { chains } = useWalletContext();
-  // const { options: chainOptions, isSingleChain } = useChains(chains);
+  const { chains } = useWalletContext();
+  const { options: chainOptions, isSingleChain } = useChains(chains);
+  const [_, setSearchParams] = useSearchParams();
 
   const rows = useMemo(() => {
     return claims?.map(claim => <HistoricalClaimsTableRow key={uuidv4()} claim={claim} />);
   }, [claims]);
 
+  const [chainIdsFilter] = useSearchParamState<string[]>(
+    "chain",
+    v => v?.join(","),
+    v => v?.split(","),
+  );
+
+  const updateChains = useCallback((key: string, value: string[], searchParams: URLSearchParams) => {
+    if (value?.length === 0 || !value) searchParams.delete(key);
+    else searchParams.set(key, value?.join(","));
+  }, []);
+
   return (
     <Group className="flex-row w-full [&>*]:flex-grow">
-      {rows?.length > 0 ? (
-        <HistoricalClaimsTable
-          responsive
-          dividerClassName={index => (index < 2 ? "bg-accent-8" : "bg-main-8")}
-          header={
-            <Group className="flex-nowrap items-center">
-              <Title h={5} look="soft">
-                History
-              </Title>
-              {/* <Select
-                // state={[chainIdsInput, n => setChainIdsInput(n)]}
+      <HistoricalClaimsTable
+        responsive
+        dividerClassName={index => (index < 2 ? "bg-accent-8" : "bg-main-8")}
+        header={
+          <Group className="flex-nowrap items-center">
+            <Title h={5} look="soft">
+              History
+            </Title>
+            {!isSingleChain && (
+              <Select
+                state={[
+                  chainIdsFilter,
+                  n =>
+                    setSearchParams(s => {
+                      updateChains("chain", n, s);
+                      return s;
+                    }),
+                ]}
                 allOption={"All chains"}
                 multiple
                 search
@@ -38,14 +60,20 @@ export default function HistoricalClaimsLibrary(props: HistoricalClaimsLibraryPr
                 look="base"
                 placeholder="Chain"
                 placeholderIcon={<Icon remix="RiLink" />}
-              /> */}
-            </Group>
-          }>
-          {rows}
-        </HistoricalClaimsTable>
-      ) : (
-        <Text>No claim transaction found</Text>
-      )}
+              />
+            )}
+          </Group>
+        }>
+        {rows?.length > 0 ? (
+          rows
+        ) : (
+          <Box className="justify-center items-center">
+            <Space size="xl" />
+            <Text>No claim transaction found</Text>
+            <Space size="xl" />
+          </Box>
+        )}
+      </HistoricalClaimsTable>
     </Group>
   );
 }
