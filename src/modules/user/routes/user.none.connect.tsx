7aafe6586feff395fd21b9146374a2e36046cc1f
sheykei <clement4saunier@gmail.com>
2025-03-03 16:40:51 +0100

    fix: responsivness

--- src/modules/user/routes/user.none.connect.tsx
+++ src/modules/user/routes/user.none.connect.tsx
@@ -19,7 +19,7 @@ export default function Index() {
     <Container>
       {Array(3).fill(<Space size="xl" />)}
       <Group className="w-full items-center flex-col">
-        <Box size="xl">
+        <Box size="xl" className="p-xl*4 min-w-[500px]">
           {/* <Space size="xl" /> */}
           <Title h={5} className="uppercase text-main-11">
             Enter an address
@@ -31,13 +31,17 @@ export default function Index() {
             size="lg"
             className="w-full"
             suffix={
-              <Button onClick={() => inputAddress && navigate(`/users/${inputAddress}`)} size="xl" look="soft">
-                <Icon remix="RiSendPlane2Fill" />
+              <Button
+                disabled={!inputAddress || inputAddress === ""}
+                onClick={() => inputAddress && navigate(`/users/${inputAddress}`)}
+                size="xl"
+                look="soft">
+                <Icon remix="RiArrowRightLine" />
               </Button>
             }
           />
           <Group className="w-full items-center flex-col">
-            <Text>Or</Text>
+            <Text>or</Text>
           </Group>
           <Group className="justify-center">
             <Connected size="lg" className="w-full justify-center" hideSpyMode={merklConfig.hideSpyMode}>
