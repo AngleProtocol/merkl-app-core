e05e5046960db26a5107b73e648042db15766888
picodes <picodes@angle.money>
2025-03-03 10:06:57 +0100

    chore: status handling

--- src/modules/status/status.service.ts
+++ src/modules/status/status.service.ts
@@ -1,8 +1,18 @@
 import { api } from "@core/api";
-import { type ApiResponse, fetchResource } from "@core/api/utils";
+import { fetchWithLogs } from "@core/api/utils";
 
 export abstract class StatusService {
-  static #fetch = <R, T extends ApiResponse<R>>(call: () => Promise<T>) => fetchResource<R, T>("Chain")(call);
+  static async #fetch<R, T extends { data: R; status: number; response: Response }>(
+    call: () => Promise<T>,
+    resource = "Delays",
+  ): Promise<NonNullable<T["data"]>> {
+    const { data, status } = await fetchWithLogs(call);
+
+    if (status === 404) throw new Response(`${resource} not found`, { status });
+    if (status === 500) throw new Response(`${resource} unavailable`, { status });
+    if (data == null) throw new Response(`${resource} unavailable`, { status });
+    return data;
+  }
 
   static async getStatusAndDelays() {
     const status = await StatusService.#fetch(async () => api.v4["campaign-status"].delay.status.get());
