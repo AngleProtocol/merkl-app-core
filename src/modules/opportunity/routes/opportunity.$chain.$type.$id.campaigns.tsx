1b501c6c158176db1298dd86db7c48aa659478d4
Hugo Lextrait <hugo.lextrait@gmail.com>
2025-03-12 12:41:19 +0100

    refactor: clean responsivness of campaigns page

--- src/modules/opportunity/routes/opportunity.$chain.$type.$id.campaigns.tsx
+++ src/modules/opportunity/routes/opportunity.$chain.$type.$id.campaigns.tsx
@@ -9,7 +9,7 @@ export default function Index() {
 
   return (
     <Container>
-      <Group className="flex-wrap flex-col-reverse lg:flex-nowrap lg:flex-row gap-xl py-xl">
+      <Group className="flex-wrap lg:flex-nowrap lg:flex-row gap-xl py-xl">
         <OpportunityBoxParticipate opportunity={opportunity} className="max-w-sm min-w-[24ch]" />
         <Group className="flex flex-col items-start gap-xl ">
           <Text className="h-[fit-content] border-1 rounded-lg border-main-5 p-xl">
