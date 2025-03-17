80d456c7aa8a40b1f5983325cd87b915eeadd605
Hugo Lextrait <hugo.lextrait@gmail.com>
2025-03-17 12:02:16 +0100

    feat: enhance OpportunityTableDailyRewards to display token breakdowns in a dropdown

--- src/modules/opportunity/components/element/OpportunityTableDailyRewards.tsx
+++ src/modules/opportunity/components/element/OpportunityTableDailyRewards.tsx
@@ -1,6 +1,8 @@
-import type { Opportunity } from "@merkl/api";
-import { type Component, Group } from "packages/dappkit/src";
+import type { Opportunity, Token as TokenType } from "@merkl/api";
+import { type Component, Dropdown, EventBlocker, Group } from "packages/dappkit/src";
 import useOpportunityRewards from "../../hooks/useOpportunityRewards";
+import Token from "@core/modules/token/components/element/Token";
+import { useMemo } from "react";
 
 export type OpportunityTableDailyRewardsProps = {
   opportunity: Opportunity;
@@ -13,9 +15,40 @@ export default function OpportunityTableDailyRewards({
 }: Component<OpportunityTableDailyRewardsProps>) {
   const { formattedDailyRewards } = useOpportunityRewards(opportunity);
 
+  const displayTokenBreakdowns = useMemo(() => {
+    if (!opportunity.rewardsRecord?.breakdowns) return null;
+
+    const mergedBreakdowns = opportunity.rewardsRecord.breakdowns.reduce<
+      Record<string, { token: TokenType; amount: bigint }>
+    >((acc, tokenBreakdown) => {
+      const { token, amount } = tokenBreakdown;
+      const key = token.address;
+
+      if (acc[key]) {
+        acc[key].amount = acc[key].amount + BigInt(amount);
+      } else {
+        acc[key] = { token, amount: BigInt(amount) };
+      }
+
+      return acc;
+    }, {});
+
+    return Object.values(mergedBreakdowns).map(({ token, amount }) => (
+      <Group key={token.id}>
+        <Token token={token} amount={amount} />
+      </Group>
+    ));
+  }, [opportunity]);
+
   return (
-    <Group className="flex-nowrap items-center" size="sm">
-      <Group className="min-w-0 flex-nowrap items-center overflow-hidden">{formattedDailyRewards}</Group>
-    </Group>
+    <EventBlocker>
+      <Dropdown
+        className="flex-nowrap items-center"
+        size="sm"
+        onHover
+        content={<Group className="flex-col p-sm">{displayTokenBreakdowns}</Group>}>
+        <Group className="min-w-0 flex-nowrap items-center overflow-hidden">{formattedDailyRewards}</Group>
+      </Dropdown>
+    </EventBlocker>
   );
 }
