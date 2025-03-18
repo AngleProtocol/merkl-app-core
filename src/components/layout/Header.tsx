f32ebff275b921973e82702710e7f3fc0858eb55
sheykei <clement4saunier@gmail.com>
2025-03-18 14:06:18 +0100

    check

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,18 +2,7 @@ import { useMerklConfig } from "@core/modules/config/config.context";
 import type { NavigationMenuRoute } from "@core/modules/config/config.model";
 import useMixpanelTracking from "@core/modules/mixpanel/hooks/useMixpanelTracking";
 import { useLocation } from "@remix-run/react";
-import {
-  Button,
-  Container,
-  Divider,
-  Group,
-  Icon,
-  Select,
-  Text,
-  WalletButton,
-  mergeClass,
-  useWalletContext,
-} from "dappkit";
+import { Button, Container, Group, Icon, Select, Text, WalletButton, mergeClass, useWalletContext } from "dappkit";
 import { motion } from "framer-motion";
 import { useEffect, useMemo, useRef, useState } from "react";
 import useChains from "../../modules/chain/hooks/useChains";
