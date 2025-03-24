636c0e703502efbb30ac7f5f5590b7cb1fdadc13
Hugo Lextrait <hugo.lextrait@gmail.com>
2025-03-24 14:56:59 +0100

    fix: update message for inactive campaigns in CampaignLibrary component

--- src/modules/campaigns/components/library/CampaignLibrary.tsx
+++ src/modules/campaigns/components/library/CampaignLibrary.tsx
@@ -61,7 +61,7 @@ export default function CampaignLibrary({ opportunity, chain }: CampaignLibraryP
           <OverrideTheme coloring={!hasOnlyInactiveCampaigns ? "good" : undefined}>
             {hasOnlyInactiveCampaigns ? (
               <Text bold look="soft">
-                NO INACTIVE CAMPAIGNS
+                There is no active campaign
               </Text>
             ) : (
               <Text bold look="soft">
