09c08f0065b9378c7c373e0e475bd1ad3a182954
sheykei <clement4saunier@gmail.com>
2025-03-24 16:16:19 +0100

    tweak: dashboarx

--- src/components/element/historicalClaimsLibrary/HistoricalClaimsLibrary.tsx
+++ src/components/element/historicalClaimsLibrary/HistoricalClaimsLibrary.tsx
@@ -58,6 +58,7 @@ export default function HistoricalClaimsLibrary(props: HistoricalClaimsLibraryPr
                 search
                 indexOptions={indexOptions}
                 options={chainOptions}
+                indexOptions={indexOptions}
                 look="base"
                 placeholder="Chain"
                 placeholderIcon={<Icon remix="RiLink" />}
