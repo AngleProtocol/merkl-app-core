c0f5f0b3d7bdd8f00c2b1d214412884e2514c947
Vincent <vincent@merkl.xyz>
2025-03-26 17:26:11 +0100

    lint

--- src/config/actions.ts
+++ src/config/actions.ts
@@ -6,63 +6,68 @@ export const actions = {
     label: "Invalid",
     description: "Invalid",
     icon: { remix: "RiFileWarningLine" },
-    cta : "Invalid"
+    cta: "Invalid",
   },
   POOL: {
     label: "Provide Liquidity",
     description: "Earn rewards by depositiong liquidity in this pool.",
     icon: { remix: "RiWaterFlashFill" },
-    cta : "Supply Liquidity"
+    cta: "Supply Liquidity",
   },
   DROP: {
     label: "Airdrop",
     description: "Earn rewards by depositiong liquidity in this pool.",
     icon: { remix: "RiWindyFill" },
-    cta : "Participate"
+    cta: "Participate",
   },
   BORROW: {
     label: "Borrow",
     description: "Earn rewards by depositiong liquidity in this pool.",
     icon: { remix: "RiTokenSwapLine" },
-    cta : "Borrow Assets"
+    cta: "Borrow Assets",
   },
   LEND: {
     label: "Lend",
     description: "Earn rewards by depositiong liquidity in this pool.",
     icon: { remix: "RiShoppingBagFill" },
-    cta : "Lend Assets"
+    cta: "Lend Assets",
   },
   HOLD: {
     label: "Hold",
     description: "Earn rewards by depositiong liquidity in this pool.",
     icon: { remix: "RiCoinFill" },
-    cta : "Acquire & Hold"
+    cta: "Acquire & Hold",
   },
   SWAP: {
     label: "Swap",
     description: "Earn rewards by swapping assets.",
     icon: { remix: "RiArrowLeftRightLine" },
-    cta : "Swap"
+    cta: "Swap",
   },
   LONG: {
     label: "Long",
     description: "Earn rewards by opening longs.",
     icon: { remix: "RiArrowLeftRightLine" },
-    cta : "Open Long"
+    cta: "Open Long",
   },
   SHORT: {
     label: "Short",
     description: "Earn rewards by opening shorts.",
     icon: { remix: "RiArrowLeftRightLine" },
-    cta : "Open Short"
+    cta: "Open Short",
   },
-} satisfies { [S in Opportunity["action"]]: { label: string; icon: IconProps; description: string, cta: string } };
+} satisfies { [S in Opportunity["action"]]: { label: string; icon: IconProps; description: string; cta: string } };
 
 export type Action = keyof typeof actions;
 
-export function getActionData(labelOrKey: string): { label: string; icon: IconProps; description: string, cta: string } | undefined {
+export function getActionData(
+  labelOrKey: string,
+): { label: string; icon: IconProps; description: string; cta: string } | undefined {
   for (const [action, data] of Object.entries(actions)) {
-    if (data.label?.toLowerCase() === labelOrKey?.toLowerCase() || action?.toLocaleLowerCase() === labelOrKey?.toLowerCase())
+    if (
+      data.label?.toLowerCase() === labelOrKey?.toLowerCase() ||
+      action?.toLocaleLowerCase() === labelOrKey?.toLowerCase()
+    )
       return data;
   }
 }
