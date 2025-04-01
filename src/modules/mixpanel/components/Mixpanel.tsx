1e428a16d0c349d1bdb0f3a2310a29e42496de91
sheykei <clement4saunier@gmail.com>
2025-04-01 16:27:37 +0200

    tweak: consent banner look

--- src/modules/mixpanel/components/Mixpanel.tsx
+++ src/modules/mixpanel/components/Mixpanel.tsx
@@ -1,5 +1,5 @@
 import { useMerklConfig } from "@core/modules/config/config.context";
-import { Box, Button, Container, Group, Icon } from "packages/dappkit/src";
+import { Box, Button, Container, Group, Icon, Text } from "packages/dappkit/src";
 import useMixpanel from "../hooks/useMixpanel";
 import useMixpanelConsent from "../hooks/useMixpanelConsent";
 import useMixpanelPage from "../hooks/useMixpanelPage";
@@ -17,9 +17,9 @@ export default function Mixpanel() {
   return (
     <>
       <div className="animate-drop fixed bottom-0 left-0 right-0 z-50 bg-black/50">
-        <Container className="mb-xl w-[100vw] mx-auto p-sm">
-          <Box className="justify-between flex-row items-center shadow-md shadow-accent-1">
-            <div>
+        <Container className="mb-xl md:max-w-[70vw] w-[100vw] mx-auto p-sm">
+          <Box className="bg-main-1 grid grid-cols-1 md:grid-cols-[1fr,min-content] items-center shadow-md shadow-accent-1">
+            <Text size="xs">
               We may collect logs of your interactions to improve our products. See{" "}
               <a
                 className="text-accent-11 dim inline-flex gap-sm"
@@ -29,12 +29,12 @@ export default function Mixpanel() {
                 privacy notice
                 <Icon remix="RiArrowRightUpLine" />
               </a>
-            </div>
-            <Group>
-              <Button onClick={() => setConsent(true)} className="rounded-md" look="hype">
+            </Text>
+            <Group size="sm" className="md:flex md:flex-nowrap grid grid-cols-2 grid-wrap">
+              <Button size="xs" onClick={() => setConsent(true)} className="rounded-md justify-center" look="hype">
                 Allow
               </Button>
-              <Button onClick={() => setConsent(false)} className="rounded-md" look="bold">
+              <Button size="xs" onClick={() => setConsent(false)} className="rounded-md justify-center" look="bold">
                 Dismiss
               </Button>
             </Group>
