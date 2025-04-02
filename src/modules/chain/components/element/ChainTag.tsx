66065ba22970a4433018ac94bf266c32e13ffb35
picodes <picodes@angle.money>
2025-04-02 17:31:34 +0200

    adding back links

--- src/modules/chain/components/element/ChainTag.tsx
+++ src/modules/chain/components/element/ChainTag.tsx
@@ -10,23 +10,16 @@ export default function ChainTag({
 }: { chain: { id: number }; suffix?: ReactNode } & PrimitiveTagProps) {
   const { chain } = useChain({ id });
 
-  // const navigate = useNavigate();
-  // const navigateToChainPage = useCallback(() => {
-  //   if (!chain) return;
-  //   navigate(`/chains/${chain.name}`);
-  // }, [chain, navigate]);
+  const navigate = useNavigate();
+  const navigateToChainPage = useCallback(() => {
+    if (!chain) return;
+    navigate(`/chains/${chain.name}`);
+  }, [chain, navigate]);
 
   if (!chain) return;
   return (
     <EventBlocker>
-      <PrimitiveTag
-        look="base"
-        key={chain.name}
-        {...props}
-        onClick={
-          onClick
-          // || navigateToChainPage
-        }>
+      <PrimitiveTag look="base" key={chain.name} {...props} onClick={onClick || navigateToChainPage}>
         <Icon size={props?.size} src={chain?.icon} />
         {chain?.name}
         {suffix}
