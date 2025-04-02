062d385a8977b0b86401679fe80b714838e11e22
hugolxt <87241914+hugolxt@users.noreply.github.com>
2025-04-02 15:17:55 +0200

    fix: remove unused count data from protocols loader (#120)

--- src/modules/protocol/routes/protocols.header.tsx
+++ src/modules/protocol/routes/protocols.header.tsx
@@ -3,21 +3,20 @@ import useMetadata from "@core/modules/metadata/hooks/useMetadata";
 import { MetadataService } from "@core/modules/metadata/metadata.service";
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { Outlet, useLoaderData } from "@remix-run/react";
-import { v4 as uuidv4 } from "uuid";
 import { I18n } from "../../../I18n";
 import Hero from "../../../components/composite/Hero";
 import { ProtocolService } from "../../../modules/protocol/protocol.service";
 
 export async function loader({ context: { backend, routes }, request }: LoaderFunctionArgs) {
-  const { protocols, count } = await ProtocolService({ api, request, backend }).getManyFromRequest();
+  const { protocols } = await ProtocolService({ api, request, backend }).getManyFromRequest();
 
-  return { protocols, count, backend, routes, ...MetadataService({ request, backend, routes }).fill() };
+  return { protocols, backend, routes, ...MetadataService({ request, backend, routes }).fill() };
 }
 
 export const meta = MetadataService({}).forwardMetadata<typeof loader>();
 
 export default function Index() {
-  const { count, url } = useLoaderData<typeof loader>();
+  const { url } = useLoaderData<typeof loader>();
   const metadata = useMetadata(url);
 
   return (
@@ -25,14 +24,7 @@ export default function Index() {
       compact
       icons={[{ remix: "RiVipCrown2Fill", className: "text-main-11 !w-lg*4 !h-lg*4" }]}
       title={I18n.trad.get.pages.protocols.title}
-      description={metadata.find(metadata.wrap(), "description")}
-      sideDatas={[
-        {
-          data: count,
-          label: "protocols",
-          key: uuidv4(),
-        },
-      ]}>
+      description={metadata.find(metadata.wrap(), "description")}>
       <Outlet />
     </Hero>
   );
