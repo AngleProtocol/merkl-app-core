1909db53e2b7fbf2a903f9b70c39b03417c70c34
picodes <picodes@angle.money>
2025-04-16 13:01:22 +0200

    fix: query issue

--- src/modules/integrations/integration.service.ts
+++ src/modules/integrations/integration.service.ts
@@ -9,11 +9,11 @@ export const IntegrationsService = defineModule<{ api: Api; request: Request; ba
 
     const getAll = inject(["api", "request"]).inFunction(({ api, request }) => {
       const url = new URL(request.url);
+      const query = {};
+      if (url.searchParams.get("chain")) query.chainId = url.searchParams.get("chain");
       return fetch(() =>
         api.v4.tokens.reward.get({
-          query: {
-            chainId: url.searchParams.get("chain") ?? undefined,
-          },
+          query,
         }),
       );
     });
