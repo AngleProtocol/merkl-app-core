acd90ab0e300ce62bad79f8aa4b518dbab5eebe5
picodes <picodes@angle.money>
2025-04-30 11:17:27 +0200

    chore: protocol metric box

--- src/modules/protocol/routes/protocol.$id.header.tsx
+++ src/modules/protocol/routes/protocol.$id.header.tsx
@@ -1,12 +1,15 @@
 import { api } from "@core/api";
-import Hero, { defaultHeroSideDatas } from "@core/components/composite/Hero";
+import Hero from "@core/components/composite/Hero";
+import { useMerklConfig } from "@core/index.generated";
 import { Cache } from "@core/modules/cache/cache.service";
 import { MetadataService } from "@core/modules/metadata/metadata.service";
+import useMixpanelTracking from "@core/modules/mixpanel/hooks/useMixpanelTracking";
+import MetricBox from "@core/modules/opportunity/components/element/MetricBox";
 import { OpportunityService } from "@core/modules/opportunity/opportunity.service";
 import useProtocolMetadata from "@core/modules/protocol/hooks/useProtocolMetadata";
 import { ProtocolService } from "@core/modules/protocol/protocol.service";
 import type { Opportunity } from "@merkl/api";
-import { Group } from "dappkit";
+import { Button, Container, Group, Icon, Value } from "dappkit";
 import { Outlet, useLoaderData } from "react-router";
 import type { LoaderFunctionArgs } from "react-router";
 
@@ -49,15 +52,55 @@ export type OutletContextProtocol = {
 };
 
 export default function Index() {
-  const { opportunities, count, protocol, liveOpportunityCount, maxApr, dailyRewards } = useLoaderData<typeof loader>();
+  const { opportunities, count, protocol, maxApr, dailyRewards } = useLoaderData<typeof loader>();
   const { icon, name, description } = useProtocolMetadata(protocol);
+  const dollarFormat = useMerklConfig(store => store.config.decimalFormat.dollar);
+  const { track } = useMixpanelTracking();
 
   return (
     <Hero
       icons={[{ src: icon }]}
-      title={<Group className="items-center">{name}</Group>}
-      description={description}
-      sideDatas={defaultHeroSideDatas(liveOpportunityCount, maxApr, Number.parseFloat(dailyRewards))}>
+      title={
+        <Group className="items-center">
+          {name}
+          <Button
+            external
+            to={protocol.url}
+            look="base"
+            onLink={() => track("Click on button", { button: "protocol_url", type: protocol.url })}>
+            <Icon remix="RiShareBoxLine" />
+          </Button>
+        </Group>
+      }
+      description={description}>
+      <Container>
+        <Group className="w-full mt-xl*2 mb-xl" size="xl">
+          <MetricBox
+            label="Opportunities"
+            value={
+              <Value format="0" size={4} className="text-main-12">
+                {count}
+              </Value>
+            }
+          />
+          <MetricBox
+            label="Daily rewards"
+            value={
+              <Value format={dollarFormat} size={4} className="text-main-12">
+                {dailyRewards}
+              </Value>
+            }
+          />
+          <MetricBox
+            label="Max APR"
+            value={
+              <Value format="0a%" size={4} className="!text-main-12">
+                {maxApr / 100}
+              </Value>
+            }
+          />
+        </Group>
+      </Container>
       <Outlet context={{ opportunities, count }} />
     </Hero>
   );
