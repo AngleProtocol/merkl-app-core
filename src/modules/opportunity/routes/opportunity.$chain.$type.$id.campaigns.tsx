a21b71cb1018b18a0f4ea0321687b49b12488ee6
Hugo Lextrait <hugo.lextrait@gmail.com>
2025-04-30 16:31:06 +0200

    lint

--- src/modules/opportunity/routes/opportunity.$chain.$type.$id.campaigns.tsx
+++ src/modules/opportunity/routes/opportunity.$chain.$type.$id.campaigns.tsx
@@ -2,14 +2,11 @@ import { api } from "@core/api";
 import type { MerklServerContext } from "@core/app/server/context";
 import { ErrorContent } from "@core/components/layout/ErrorContent";
 import { ChainService } from "@core/modules/chain/chain.service";
-import { InteractionService } from "@core/modules/interaction/interaction.service";
 import type { OutletContextOpportunity } from "@core/modules/opportunity/routes/opportunity.$chain.$type.$id.header";
-import type { Opportunity } from "@merkl/api";
 import { Container, Group } from "dappkit";
 import { Outlet, useLoaderData, useOutletContext } from "react-router";
 import type { LoaderFunctionArgs } from "react-router";
 import OpportunityBoxParticipate from "../components/element/OpportunityBoxParticipate";
-import { OpportunityService } from "../opportunity.service";
 
 export async function loader({
   context: { backend },
@@ -28,7 +25,7 @@ export async function loader({
   // const targets = await InteractionService({ api, backend }).getTargetsByOpportunity(opportunity as Opportunity);
 
   return {
-    targets :[],
+    targets: [],
   };
 }
 
